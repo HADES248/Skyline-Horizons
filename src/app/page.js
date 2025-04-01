499003de9b1c000b1f735728d784f8395c3732bd
HADES248 <shivasingh24803@gmail.com>
2025-04-01 18:10:07 +0530

    refactor footer component for improved styling and navigation links

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer';
 export default function Home() {
   return (
     <>
@@ -6,7 +7,6 @@ export default function Home() {
         <div className="md:w-1/2 flex justify-center items-center flex-col ml-4 md:ml-10 pr-6">
           <h1 className="text-xl md:text-2xl mt-5">Welcome to Skyline Horizons</h1>
           <form className="max-w-md w-full mt-5 rounded-lg">
-            <label htmlFor="default-search" className="mb-2 text-sm font-medium sr-only text-white">Search</label>
             <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -17,31 +17,33 @@ export default function Home() {
             </div>
           </form>
           <div className="flex justify-between flex-col sm:flex-row gap-8 mt-16">
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-start justify-center">
               <dt className="mb-2 text-3xl font-extrabold">1000+</dt>
               <dd>Premium Houses</dd>
             </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-start justify-center">
               <dt className="mb-2 text-3xl font-extrabold">80%</dt>
               <dd >Properties Sold!</dd>
             </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-start justify-center">
               <dt className="mb-2 text-3xl font-extrabold">20+</dt>
               <dd >Awards Won</dd>
             </div>
           </div>
         </div>
       </div>
-      <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 justify-items-center mt-20 mb-10">
-        <img src="no-broker.svg" className="w-[190px] lg:w-[220px]" />
-        <img src="realty.png" className="w-[190px] lg:w-[220px]" />
-        <img src="zillow.svg" className="w-[190px] lg:w-[220px]" />
-        <img src="equinix.png" className="w-[190px] lg:w-[220px]" />
+      <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 justify-items-center mt-14 mb-10">
+        <img src="no-broker.svg" className="w-[180px] lg:w-[220px]" />
+        <img src="realty.png" className="w-[180px] lg:w-[220px]" />
+        <img src="zillow.svg" className="w-[180px] lg:w-[220px]" />
+        <img src="equinix.png" className="w-[180px] lg:w-[220px]" />
       </div>
       <div className="container">
         <h1 className="font-bold mt-10 mb-12 ml-7 sm:ml-10 underline underline-offset-2 decoration-4 decoration-primary text-white tracking-wide">
           Most Popular Residences
         </h1>
+
+        {/* Houses */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6 justify-items-center ml-4 mr-4">
           <div className="card flex flex-col max-w-sm bg-gradient-to-l from-primary via-blue-400 to-primary rounded-2xl shadow-lg text-white hover:shadow-2xl cursor-pointer transform hover:scale-105 transition-all duration-300 ease-in-out">
             <img className="rounded-t-2xl object-cover h-48" src="front-page.jpg" />
@@ -76,8 +78,70 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
 
+        {/* Reviews */}
+        <div className="m-4">
+          <h1 className="flex justify-center items-center font-bold mb-7 underline underline-offset-2 decoration-4 decoration-primary text-white tracking-wide">
+            Reviews
+          </h1>
+
+          <div className="grid mb-8 shadow-lg md:shadow-xs rounded-xl bg-gradient-to-br from-primary to-cyan-600 md:mb-12 md:grid-cols-2">
+            <figure className="flex flex-col items-center justify-center p-8 text-center border-b rounded-tl-lg md:rounded-ss-lg md:border-e border-gray-300 bg-transparent">
+              <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8 text-gray-300">
+                <h3 className="text-lg font-semibold text-white">Exceptional User Experience and Intuitive Design</h3>
+                <p className="my-2">The website offers a seamless browsing experience with user-friendly navigation. It simplifies property searches effectively."</p>
+              </blockquote>
+              <img src="rating-45.png" className="w-36 mb-8" />
+              <figcaption className="flex items-center justify-center">
+                <div className="font-medium text-white">
+                  <div>Jack Quad</div>
+                  <div className="text-sm text-gray-300">Buyer</div>
+                </div>
+              </figcaption>
+            </figure>
+            <figure className="flex flex-col items-center justify-center p-8 text-center border-b md:rounded-se-lg border-gray-300 bg-transparent">
+              <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8 text-gray-300">
+                <h3 className="text-lg font-semibold text-white">Solid foundation for any transaction</h3>
+                <p className="my-2">Whether you're buying or renting a property, their procedure is seamless!</p>
+              </blockquote>
+              <img src="rating-50.png" className="w-36 mb-8" />
+              <figcaption className="flex items-center justify-center">
+                <div className=" font-medium text-white ms-3">
+                  <div>Willam Dafoe</div>
+                  <div className="text-sm text-gray-300">Buyer</div>
+                </div>
+              </figcaption>
+            </figure>
+            <figure className="flex flex-col items-center justify-center p-8 text-center border-b md:rounded-es-lg md:border-b-0 md:border-e border-gray-300 bg-transparent">
+              <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8 text-gray-300">
+                <h3 className="text-lg font-semibold text-white">Comprehensive Property Information and Transparency</h3>
+                <p className="my-2">Provides accurate details about properties. The transparency builds trust and ensures informed decision-making for buyers and renters."</p>
+              </blockquote>
+              <img src="rating-50.png" className="w-36 mb-8" />
+              <figcaption className="flex items-center justify-center">
+                <div className="font-medium text-white">
+                  <div>Chandler Bing</div>
+                  <div className="text-sm text-gray-300">Renter</div>
+                </div>
+              </figcaption>
+            </figure>
+            <figure className="flex flex-col items-center justify-center p-8 text-center rounded-b-lg md:rounded-se-lg bg-tranparent">
+              <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8 text-gray-300">
+                <h3 className="text-lg font-semibold text-white">Efficient Collaborating</h3>
+                <p className="my-2">Facilitates direct communication with property owners and agents. This feature saves time and eliminates unnecessary brokerage fees."</p>
+              </blockquote>
+              <img src="rating-40.png" className="w-36 mb-8" />
+              <figcaption className="flex items-center justify-center">
+                <div className="font-medium text-white">
+                  <div>Joey Tribbiani</div>
+                  <div className="text-sm text-gray-300">Property Seller</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,34 @@
+'use client';
 import Link from "next/link";
 
 export default function Navbar() {
+
+  const navFunction = () => {
+    const ham = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+    if (menu.classList.contains('hidden')) {
+      ham.classList.remove('rotate-180');
+      menu.classList.remove('hidden');
+    } else {
+      ham.classList.add('rotate-180');
+      menu.classList.add('hidden');
+    }
+  }
+
   return (
     <nav className="bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="self-center sm:text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-cyan-300 to-85% to-primary bg-clip-text text-transparent">Skyline Horizons</Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link href="/login" type="button" className="text-white font-medium rounded-lg text-sm px-4 py-2 text-center cursor-pointer bg-primary hover:bg-blue-700">Login</Link>
-          <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
+          <button type="button" id="hamburger" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden text-gray-400 hover:bg-gray-700 transition-all ease-in-out duration-300" onClick={navFunction}>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
         </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="menu">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
-            <li>
-              <Link href="/" className="block py-2 px-3 md:p-0 text-white bg-primary rounded-sm md:bg-transparent md:text-primary" aria-current="page">Home</Link>
-            </li>
             <li>
               <Link href="/about" className="block py-2 px-3 md:p-0 rounded-sm md:hover:text-primary text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">About</Link>
             </li>
