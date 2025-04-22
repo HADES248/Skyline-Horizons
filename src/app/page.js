d78635bae6facf1228e341a660d83bf43a41a8b5
HADES248 <shivasingh24803@gmail.com>
2025-04-22 12:27:37 +0530

    refactor layout for property display; adjust grid columns and image dimensions in PropertyCard

--- src/app/page.js
+++ src/app/page.js
@@ -81,7 +81,7 @@ export default function Home() {
         </h1>
 
         {/* Houses */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6 justify-items-center ml-6 mr-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 justify-items-center ml-6 mr-6">
 
           {/* Loading Houses */}
           <Card />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -54,7 +54,7 @@ export default function PropertyCard() {
         properties.map((property) => (
           <Link href={`/${property._id}`} className="flex flex-col w-full bg-gradient-to-l from-primary via-blue-400 to-primary rounded-2xl shadow-lg text-white hover:shadow-2xl cursor-pointer transform hover:scale-105 transition-all duration-300 ease-in-out" key={property._id}>
             <Image
-              className="rounded-t-2xl object-cover h-56"
+              className="rounded-t-2xl object-cover h-[3/4] w-full"
               src="/front-page.jpg"
               alt="House1"
               width={500}
@@ -63,7 +63,7 @@ export default function PropertyCard() {
             <div className="p-6">
               <span className="block text-xl font-semibold text-gray-100 mb-2">₹ {property.price}</span>
               <h2 className="text-3xl font-bold mb-3">{property.name}</h2>
-              <p className="text-lg text-gray-200">{property.description.slice(0, 100)}..</p>
+              <p className="text-lg text-gray-200">{property.description.slice(0, 60)}..</p>
             </div>
           </Link>
         ))
