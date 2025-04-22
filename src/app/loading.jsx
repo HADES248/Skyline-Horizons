48e84d9324c9ea0a2fe2ff4bdc308b4142785b11
HADES248 <shivasingh24803@gmail.com>
2025-04-22 11:58:53 +0530

    Fixing loading page.

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,8 @@
 export default function loading() {
   return (
     <main>
-      "Loading..."
+      <p className="text-center text-xl">Wait a second...</p>
+      <p className="text-center text-lg">This shouldn&#39;t take Long... :)</p>
     </main>
   )
 }
