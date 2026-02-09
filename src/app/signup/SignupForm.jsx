0d885d49a973451f34ef1325ebda464ab8c88b23
HADES248 <shivasingh24803@gmail.com>
2026-02-09 14:55:12 +0530

    feat: Integrate better-auth for user authentication and add Google sign-in functionality

--- src/app/signup/SignupForm.jsx
+++ src/app/signup/SignupForm.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { UserContext } from "@/context/user";
+import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { useContext, useState } from "react";
 
@@ -44,6 +45,14 @@ export default function SignupForm() {
         setUserExists(true);
       }
     }
+  }
+
+  const handleGoogleSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+
+    console.log(data);
   }
 
   return (
@@ -93,7 +102,11 @@ export default function SignupForm() {
         <div className="block mb-2">OR</div>
       </div>
       <div className="flex flex-col sm:flex-row justify-center items-center">
-        <button type="button" className="text-white hover:text-gray-800 bg-primary hover:bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center focus:ring-[#4285F4]/55 me-2 mb-2 cursor-pointer">
+        <button
+          type="button"
+          className="text-white hover:text-gray-800 bg-primary hover:bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center focus:ring-[#4285F4]/55 me-2 mb-2 cursor-pointer"
+          onClick={handleGoogleSignIn}
+        >
           <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
             <path fillRule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clipRule="evenodd" />
           </svg>
